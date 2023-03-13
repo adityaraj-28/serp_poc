4404e1434f628d6ce6c296cb5d5e8c7affb5fcd6
Aditya Raj <aditya.raj@synaptic.com>
2023-03-13 18:43:30 +0530

    complete linkedin end-to-end flow

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
 import pandas as pd
 import requests
 from bs4 import BeautifulSoup
+from urllib.parse import urlparse
 
 SERP_DIV_CLASS = "yuRUbf"
 
 
-def extract_domains_from_linkedin():
+def extract_websites_from_linkedin():
     soup = BeautifulSoup(unblocker_res.text, 'html.parser')
     tag = soup.find('a', {'data-tracking-control-name': 'about_website'})
     text = tag.text.strip()
@@ -13,8 +14,20 @@ def extract_domains_from_linkedin():
     return website_list
 
 
+def serp_datasource_id_from_linkedin_url(url):
+    if url[-1] == '/':
+        url = url[:-1]
+    path = urlparse(url).path
+    _domain = path.split('/')[-1]
+    return _domain
+
+
 unblocker_dict = {
-    'linkedin': extract_domains_from_linkedin
+    'linkedin': extract_websites_from_linkedin
+}
+
+datasource_serp_id_extractor = {
+    'linkedin': serp_datasource_id_from_linkedin_url
 }
 
 
@@ -28,30 +41,51 @@ def extract_url_from_serp_res():
     return unblocker_url_list
 
 
+def create_output_file_entry():
+    extracted_domain = urlparse(website).netloc
+    domain_matching = 'True' if domain == extracted_domain else 'False'
+    _row = f'{company_id}, {data_source}, {domain}, {unblocker_url}, {serp_data_source_id}, {website}, {extracted_domain}, {domain_matching}\n'
+    file_name_dict[data_source].write(_row)
+
+
 if __name__ == '__main__':
     df = pd.read_csv('serp_sample.csv')
-    columns = list(df.columns)
+    associations = ['linkedin', 'glassdoor', 'pitchbook', 'playstore', 'appstore']
+    file_name_dict = {}
+    for association in associations:
+        file_name_dict[association] = open(f'{association}.txt', 'w+')
+    error_file = open('errors.txt', 'w+')
+    session = requests.Session()
     # required columns company_id, entity, domain
     for index, row in df.iterrows():
         domain = row['domain']
         company_id = row['company_id']
-        # association
         data_source = row['entity']
         google_query = row['google_query']
+        try:
+            serp_url = "https://www.google.com/search?q=" + google_query.replace(' ', '+')
 
-        serp_url = "https://www.google.com/search?q=" + google_query.replace(' ', '+')
+            serp_proxies = {
+                "https": "https://brd-customer-hl_387a0b46-zone-serp_zone:7j5tinf2e6il@zproxy.lum-superproxy.io:22225",
+            }
 
-        serp_proxies = {
-            "https": "https://brd-customer-hl_387a0b46-zone-serp_zone:7j5tinf2e6il@zproxy.lum-superproxy.io:22225",
-        }
+            response = session.get(serp_url, proxies=serp_proxies, verify=False)
+            # it returns 10 results by default
+            unblocker_urls = extract_url_from_serp_res()
+            unblocker_proxies = {
+                'https': 'http://brd-customer-hl_387a0b46-zone-unblocker_1:y1ibmxaapy29@zproxy.lum-superproxy.io:22225'
+            }
+            for unblocker_url in unblocker_urls:
+                unblocker_res = session.get(unblocker_url, proxies=unblocker_proxies, verify=False)
+                serp_data_source_id = datasource_serp_id_extractor[data_source](unblocker_url)
+                websites = unblocker_dict[data_source]()
+                for website in websites:
+                    create_output_file_entry()
+            break
+        except Exception as e:
+            error_file.write(f'{company_id}, {company_id}, {data_source}, {google_query} : {str(e)}\n')
 
-        response = requests.get(serp_url, proxies=serp_proxies, verify=False)
-        # it returns 10 results by default
-        unblocker_urls = extract_url_from_serp_res()
-        unblocker_proxies = {
-            'https': 'http://brd-customer-hl_387a0b46-zone-unblocker_1:y1ibmxaapy29@zproxy.lum-superproxy.io:22225'
-        }
-        for unblocker_url in unblocker_urls:
-            unblocker_res = requests.get(unblocker_url, proxies=unblocker_proxies, verify=False)
-            unblocker_dict[data_source](unblocker_res)
-        break
+    session.close()
+    error_file.close()
+    for file in file_name_dict.values():
+        file.close()
